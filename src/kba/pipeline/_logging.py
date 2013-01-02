4d18902e86b0962773ad68aada8249c60129fe0b
John R. Frank <jrf@diffeo.com>
2013-01-01 22:55:51 -0500

    making full chunk logging for forensics a configurable option

--- src/kba/pipeline/_logging.py
+++ src/kba/pipeline/_logging.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python
+'''
+Logging utilities for the kba.pipeline
 
+This software is released under an MIT/X11 open source license.
 
+Copyright 2012 Diffeo, Inc.
+'''
 
 import os
 import streamcorpus
 
-def log_full_file(stream_item, extension, log_path='/data/trec-kba/data/wlc-streamcorpus-logs'):
+def log_full_file(stream_item, extension, log_path=None):
     '''
     Saves a full stream_item to a one-file-long chunk with a name
     'extension' added to help explain why it was saved.
     '''
-    fname = '%s-%s.sc' % (stream_item.stream_id, extension)
-    fpath = os.path.join(log_path, fname)
-    l_chunk = streamcorpus.Chunk(path=fpath, mode='wb')
-    l_chunk.add(stream_item)
-    l_chunk.close()
+    if log_path and stream_item and extension:
+        fname = '%s-%s.sc' % (stream_item.stream_id, extension)
+        fpath = os.path.join(log_path, fname)
+        l_chunk = streamcorpus.Chunk(path=fpath, mode='wb')
+        l_chunk.add(stream_item)
+        l_chunk.close()
 
-    print 'created %s' % fpath
+        print 'created %s' % fpath
