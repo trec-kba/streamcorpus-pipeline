8d6870a0a3c65132e83649ef0b2d618d57749c4e
John R. Frank <jrf@diffeo.com>
2013-02-06 21:39:09 -0500

    making richer tests for hyperlink_label generation, which will be needed more for the WLC data

--- src/kba/pipeline/test_hyperlink_labels.py
+++ src/kba/pipeline/test_hyperlink_labels.py
@@ -1,6 +1,19 @@
+import os
+from streamcorpus import StreamItem, ContentItem
+from _hyperlink_labels import anchors_re, hyperlink_labels
 
-
-from _hyperlink_labels import anchors_re
+def test_basics():
+    stream_item = StreamItem()
+    stream_item.body = ContentItem()
+    path = os.path.dirname(__file__)
+    path = os.path.join( path, '../../../data/test/' )
+    stream_item.body.clean_html = open(
+        os.path.join(path, 'nytimes-index-clean.html')).read()
+    stream_item = hyperlink_labels(
+        {'require_abs_url': True, 
+         'domain_substrings': ['nytimes.com']}
+        )(stream_item)
+    assert len(stream_item.body.labelsets[0].labels) == 373
 
 sample_text = u'''
 This is a normal <a href="http://en.wikipedia.org/wiki/Hello">link</a>.
@@ -8,6 +21,10 @@ This is a normal <a href="http://en.wikipedia.org/wiki/Hello">link</a>.
 This is a funky <a
 href ='http://en.wikipedia.org/wiki/Bogus'  asdf=4
 >the Bogus</a>
+
+              Obviously intrigued by anything named Munn I sought <a href="http://en.wikipedia.org/wiki/Munny">more 
+              info</a>.</font></p>
+
 '''
 
 def test_anchors_re():
@@ -19,8 +36,8 @@ def test_anchors_re():
         posthref = m.group('posthref')
         preequals = m.group('preequals')
         postequals = m.group('postequals')
-        print m.groups('href')
+        #print m.groups('href')
 
     ## now we check more of the output
-    assert len(matches) == 2
+    assert len(matches) == 3
 
