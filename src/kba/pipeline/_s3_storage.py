7f5b30c3f935dbe64799e7fbf08d7e7696b9268b
John R. Frank <jrf@diffeo.com>
2013-02-06 21:37:06 -0500

    extending _s3_storage stages to handle both streamcorpus and spinn3r formats as input.  Also implementing a separate file for holding keys that gain access to the buckets.  Also implementing verification step in the to_s3_chunks that pulls the files through their web link to verify that the content comes out as expected.

--- src/kba/pipeline/_s3_storage.py
+++ src/kba/pipeline/_s3_storage.py
@@ -11,9 +11,11 @@ import os
 import sys
 import time
 import hashlib
+import requests
 import traceback
+import _extract_spinn3r
 import streamcorpus
-from streamcorpus import decrypt_and_uncompress, compress_and_encrypt
+from streamcorpus import decrypt_and_uncompress, compress_and_encrypt, Chunk
 from cStringIO import StringIO
 
 _message_versions = {
@@ -43,32 +45,43 @@ def _retry(func):
                 break
             except Exception, exc:
                 time.sleep(3 * tries)
-                print('having trouble writing: %s' % traceback.format_exc(exc))
+                print('having I/O trouble with S3: %s' % traceback.format_exc(exc))
                 tries += 1
                 if tries > self.config['tries']:
                     log('giving up on uploading: %s' % output_path)
                     sys.exit('giving up on uploading: %s' % output_path)
     return retry_func
 
+def get_bucket(config):
+    ## use special keys for accessing AWS public data sets bucket
+    aws_access_key_id = open(config['aws_access_key_id']).read()
+    aws_secret_access_key = open(config['aws_secret_access_key']).read()
+    conn = S3Connection(aws_access_key_id,
+                        aws_secret_access_key)
+    bucket = conn.get_bucket(config['bucket'])
+    return bucket
+
 class from_s3_chunks(object):
     def __init__(self, config):
         self.config = config
-        ## use special keys for accessing AWS public data sets bucket
-        conn = S3Connection(self.config['aws_access_key_id'],
-                            self.config['aws_secret_access_key'])
-        self.bucket = conn.get_bucket(self.config['bucket'])
+        self.bucket = get_bucket(config)
 
     def __call__(self, i_str):
         '''
         Takes a date_hour string over stdin and generates chunks from
         s3://<bucket><prefix_path>/data_hour/
         '''
-        date_hour = i_str.strip()
-        print date_hour
-        for key in self.get_keys(date_hour):
-            print key.key
-            if key.key.endswith('xz.gpg'):
-                yield self.get_chunk(key)
+        if self.config['task_type'] == 'date_hour':
+            date_hour = i_str.strip()
+
+            for key in self.get_keys(date_hour):
+                print key.key
+                if key.key.endswith('xz.gpg'):
+                    yield self.get_chunk(key)
+
+        elif self.config['task_type'] == 'full_path':
+            key = Key(self.bucket, i_str.strip())
+            yield self.get_chunk(key)
 
     @_retry
     def get_keys(self, date_hour):
@@ -85,42 +98,116 @@ class from_s3_chunks(object):
         fh = StringIO()
         key.get_contents_to_file(fh)
         data = fh.getvalue()
-        _errors, data = decrypt_and_uncompress(data, self.config['gpg_key'], self.config['gpg_dir'])
+        _errors, data = decrypt_and_uncompress(data, self.config['gpg_decryption_key'], self.config['gpg_dir'])
         print '\n'.join(_errors)
-        i_content_md5 = key.key.split('.')[-3]
+        if self.config['input_format'] == 'streamitem' and \
+                self.config['streamcorpus_version'] == 'v0_1_0':
+            i_content_md5 = key.key.split('.')[-3]
+        else:
+            ## go past {sc,protostream}.xz.gpg
+            i_content_md5 = key.key.split('.')[-4]
         f_content_md5 = hashlib.md5(data).hexdigest()
         if i_content_md5 != f_content_md5:
             msg = 'FAIL: %s --> %s' % (key.key, f_content_md5)
             print(msg)
             sys.exit(msg)
 
-        message = _message_versions[ self.config['streamcorpus_version'] ]
+        if self.config['input_format'] == 'spinn3r':
+            ## convert the data from spinn3r's protostream format
+            return _extract_spinn3r._generate_stream_items( data )
 
-        return streamcorpus.Chunk(data=data, message=message)
+        elif self.config['input_format'] == 'streamcorpus':
+            message = _message_versions[ self.config['streamcorpus_version'] ]
+
+            return streamcorpus.Chunk(data=data, message=message)
+
+        else:
+            sys.exit('Invalid config: input_format = %r' % self.config['input_format'])
 
 class to_s3_chunks(object):
     def __init__(self, config):
         self.config = config
-        ## use special keys for accessing AWS public data sets bucket
-        conn = S3Connection(self.config['aws_access_key_id'],
-                            self.config['aws_secret_access_key'])
-        self.bucket = conn.get_bucket(self.config['bucket'])
+        self.bucket = get_bucket(config)
 
     def __call__(self, t_path, name_info, i_str):
-        date_hour = i_str.strip()
+        '''
+        Load chunk from t_path and put it into the right place in s3
+        using the output_name template from the config
+        '''
+        data = open(t_path).read()
+        _errors, data = compress_and_encrypt(
+            data, self.config['gpg_encryption_key'], self.config['gpg_dir'])
+        print '\n'.join(_errors)
+
+        date_hours = set()
+        for si in Chunk(data=data):
+            if not (si.stream_time and si.stream_item.zulu_timestamp):
+                open('/tmp/foo', 'wb').write(data)
+                print 'failed', repr(si)
+
+        for si in Chunk(path=t_path):
+            if not (si.stream_time and si.stream_item.zulu_timestamp):
+                open('/tmp/foo', 'wb').write(data)
+                print 'failed', repr(si)
+
+        print 'done'
+        sys.exit()
+
+        if True:
+            
+            date_hours.add( si.stream_time.zulu_timestamp[:13] )
+
+        assert len(date_hours) == 1, \
+            'got a chunk with other than one data_hour! ' + \
+            repr(date_hours)
+
+        date_hour = list(date_hours)[0]
+                
+        if self.config['task_type'] == 'date_hour':
+            expected_date_hour = i_str.strip()
+            assert date_hour == expected_date_hour, \
+                (date_hour, expected_date_hour)
+
         name_info['date_hour'] = date_hour
         o_fname = self.config['output_name'] % name_info
         o_path = os.path.join(self.config['path_prefix'], o_fname + '.sc.xz.gpg')
-        print o_path
+        print 'o_path: ', o_path
+        print 't_path: ', t_path
 
-        data = open(t_path).read()
-        _errors, data = compress_and_encrypt(data, self.config['gpg_key'], self.config['gpg_dir'])
-        print '\n'.join(_errors)
+        while 1:
+            self.put(o_path, data)
 
-        self.put(o_path, data)
+            if self.config['verify_via_http']:
+                try:
+                    self.verify(o_path, name_info['md5'])
+                    break
+                except Exception, exc:
+                    print 'verify_via_http failed so retrying: %r' % exc
+                    ## keep looping if verify raises anything
+                    continue
+
+            else:
+                ## not verifying, so don't attempt multiple puts
+                break
 
     @_retry
     def put(self, o_path, data):
         key = Key(self.bucket, o_path)
         key.set_contents_from_file(StringIO(data))
         key.set_acl('public-read')
+
+    @_retry
+    def verify(self, o_path, md5):
+        key = Key(self.bucket, o_path)
+        url = key.generate_url()
+        print('fetching %r' % url)
+        req = requests.get(url)
+        errors, data = decrypt_and_uncompress(
+            req.content, 
+            self.config['gpg_decryption_key'], self.config['gpg_dir'])
+        rec_md5 = hashlib.md5(data).hexdigest()
+        if md5 == rec_md5:
+            return
+        else:
+            print errors
+            raise Exception('original md5 = %r != %r = received md5' % (md5, rec_md5))
