007582e59bb0a9cbb477c7fbf38976e3c3de3b99
John R. Frank <jrf@diffeo.com>
2013-02-10 01:32:18 -0500

    scripts for running the pipeline more generally -- only the .submit file must be copied and changed for each type of run

---
+++ scripts/run_pipeline.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/data/trec-kba/installs/so
+
+## if java is not in a normal place, then extend the path:
+export PATH=$PATH:/bin:/usr/bin:/afs/csail/amd64_linux26/java/latest/bin
+
+## setup our python virtualenv
+source /data/trec-kba/installs/py27env/bin/activate
+
+tries=0
+while [ "$tries" -le "10" ]; do
+  echo "attempting to run " `which python`
+  python -c "import md5"
+
+  ## if that succeeded, then break out... otherwise sleep and loop
+  if [ "$?" = "0" ]; then
+    echo "successfully loaded md5"
+    break
+  else
+    echo $tries "runner failed with: " $?
+    let tries=$tries+1
+    sleep 4
+  fi
+done;
+
+## this will have to be modified for any other environments
+python -c "import sys; assert sys.executable == '/data/trec-kba/installs/py27env/bin/python'"
+if [ "$?" -ne "0" ]; then
+  exit $?
+else
+  ## everything is determined by the config file passed in as $1
+  python -m kba.pipeline.run  $1
+fi
