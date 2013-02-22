c8e83ec890305d3ea9f4c657916dd7f1c117ac26
John R. Frank <jrf@diffeo.com>
2013-02-21 20:30:31 -0500

    making test to verify that lxml clean html is working correctly

--- src/kba/pipeline/test_clean_html.py
+++ src/kba/pipeline/test_clean_html.py
@@ -2,6 +2,7 @@ import os
 import pytest
 
 from _clean_html import make_clean_html
+from _clean_visible import make_clean_visible
 
 def test_make_clean_html():
     path = os.path.dirname(__file__)
@@ -20,3 +21,18 @@ id="comments-template"><h3 id="comments">4 Responses to &#822050+ Years of Digis
 
     cleaned =  make_clean_html(test_bad_html)
     assert cleaned == correct_test_bad_html, cleaned
+
+def test_target_parsing():
+    path = os.path.dirname(__file__)
+    path = os.path.join( path, '../../../data/test/' )
+    test_html = open(os.path.join(path, 'target-test.html')).read()
+
+    html = make_clean_html( test_html )
+
+    assert 'logo' in html
+    assert 'target' in html
+
+    visible = make_clean_visible( html )
+    
+    assert 'logo' not in visible
+    assert 'target' not in visible
