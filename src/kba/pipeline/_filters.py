9e0e7850d9c9c26a8d05eba779341747f1585a91
John R. Frank <jrf@diffeo.com>
2013-03-12 23:15:32 -0400

    adding new stage for debugging purposes:  just lets specific stream_ids down the pipeline

---
+++ src/kba/pipeline/_filters.py
@@ -0,0 +1,13 @@
+'''
+incremental transforms that only pass specific StreamItems
+'''
+
+def debug_filter(config):
+    def _debug_filter(si):
+        
+        if si.stream_id in config['accept_stream_ids']:
+            return si
+        else:
+            return None
+
+    return _debug_filter
