147f2a1aa1f558ba6ec262bb7b838a2c417cdd2c
John R. Frank <jrf@diffeo.com>
2013-03-14 15:44:50 -0400

    splitting out _test_data so can use in multiple tests

---
+++ src/kba/pipeline/_test_data.py
@@ -0,0 +1,12 @@
+
+import os
+from streamcorpus import Chunk
+
+def get_test_chunk_path():
+    path = os.path.dirname(__file__)
+    path = os.path.join( path, '../../../data/test/', 'WEBLOG-100-fd5f05c8a680faa2bf8c55413e949bbf.sc' )
+    return path
+
+def get_test_chunk():
+    return Chunk(path=get_test_chunk_path())
+    return stream_item
--- src/kba/pipeline/test_dedup.py
+++ src/kba/pipeline/test_dedup.py
@@ -16,14 +16,7 @@ ch.setLevel( logging.DEBUG )
 #ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-def get_test_chunk_path():
-    path = os.path.dirname(__file__)
-    path = os.path.join( path, '../../../data/test/', 'WEBLOG-100-fd5f05c8a680faa2bf8c55413e949bbf.sc' )
-    return path
-
-def get_test_chunk():
-    return Chunk(path=get_test_chunk_path())
-    return stream_item
+from _test_data import get_test_chunk_path, get_test_chunk
 
 def test_dedup_debugging_config():
     
