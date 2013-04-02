3bcb6f3c861b5f4dd8a45532e1e9cef9f02daf6e
John R. Frank <jrf@diffeo.com>
2013-04-02 12:57:07 -0400

    expanding the "find" transform stage so that it acts as a filter and can camouflage special StreamItems by also passing random other items when they come through

--- src/kba/pipeline/_find.py
+++ src/kba/pipeline/_find.py
@@ -13,25 +13,48 @@ Copyright 2012 Diffeo, Inc.
 '''
 import os
 import uuid
+import random
+import logging
+logger = logging.getLogger(__name__)
 
 class find(object):
     def __init__(self, config):
         self._config = config
-        self._stream_ids = set(open(config['list_of_stream_ids_path']).read().splitlines())
+        self._stream_ids = set()
+        for line in open(config['list_of_stream_ids_path']).read().splitlines():
+            source, stream_id = line.split()
+            self._stream_ids.add(stream_id.strip())
+        logger.critical('loaded %d stream_ids, e.g. %s' % (len(self._stream_ids), list(self._stream_ids)[0]))
         _path = os.path.join(config['dump_path'], str(uuid.uuid1()) + '.txt')
         _parent_dir = os.path.dirname(_path)
         if not os.path.exists(_parent_dir):
             os.makedirs(_parent_dir)
         self._dump_fh = open(_path, 'wb')
+        
+        self._special_chunks = set()
 
     def __call__(self, si, context):
 
         if si.stream_id in self._stream_ids:
 
+            self._special_chunks.add( context['i_str'] )
+
             rec = [si.stream_id, context['i_str']]
 
-            line = '\t'.join(rec)
+            line = '\t'.join(rec) + '\n'
             self._dump_fh.write(line)
             self._dump_fh.flush()
 
-        return si
+            ## pass through all stream_ids that match
+            return si
+
+        elif context['i_str'] in self._special_chunks and \
+                random.random() < self._config.get('camouflage_fraction'):
+            ## for chunks that contain found stream_id, we can pass a
+            ## fraction of the remaining StreamItems as camouflage.
+            return si
+
+        elif not self._config.get('filter', True):
+            ## even it isn't one of the special stream_ids, we might
+            ## still let it three if config['filter'] == False
+            return si
