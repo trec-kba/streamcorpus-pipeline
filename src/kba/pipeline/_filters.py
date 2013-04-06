1783476d61d5d1e42bea5779cd8555e7471c6a7b
John R. Frank <jrf@diffeo.com>
2013-04-06 15:55:07 -0400

    exclusion_filter removes stream_ids that are so problematic that we do not want to see them ever again.

--- src/kba/pipeline/_filters.py
+++ src/kba/pipeline/_filters.py
@@ -11,3 +11,14 @@ def debug_filter(config):
             return None
 
     return _debug_filter
+
+def exclusion_filter(config):
+    def _exclustion_filter(si, context):
+        
+        if si.stream_id in config['excluded_stream_ids']:
+            return None
+        else:
+            return si
+
+    return _exclustion_filter
+
