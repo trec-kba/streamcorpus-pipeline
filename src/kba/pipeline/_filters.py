a41544d71ccac130b5ee59143d197de561cd2c54
John R. Frank <jrf@diffeo.com>
2013-04-07 16:22:03 -0400

    creating "filter_languages" and "remove_raw" so we can project out just the english-and-unknown-language clean_visible portion of the corpus

--- src/kba/pipeline/_filters.py
+++ src/kba/pipeline/_filters.py
@@ -13,12 +13,38 @@ def debug_filter(config):
     return _debug_filter
 
 def exclusion_filter(config):
-    def _exclustion_filter(si, context):
+    def _exclusion_filter(si, context):
         
         if si.stream_id in config['excluded_stream_ids']:
             return None
         else:
             return si
 
-    return _exclustion_filter
+    return _exclusion_filter
+
+def filter_languages(config):
+    def _filter_languages(si, context):
+        if not si.body.language and config.get('allow_null_language', True):
+            return si
+
+        if si.body.language.code in config.get('included_language_codes', []):
+            return si
+
+        ## otherwise return None, which excludes the language
+
+    return _filter_languages
+
+def remove_raw(config):
+    def _remove_raw(si, context):
+        if config.get('if_clean_visible_remove_raw', False):
+            if si.body.clean_visible:
+                si.body.raw = ''
+            ## otherwise leave body.raw unchanged
+
+        else:
+            si.body.raw = ''
+
+        return si
+
+    return _remove_raw
 
