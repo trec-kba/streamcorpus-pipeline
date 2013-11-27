e019c55443d7ff7c85f4f994b1f6e12e0ab0481c
John R. Frank <jrf@computableinsights.com>
2013-11-27 17:07:52 -0500

    using fixed timestamps and giving a much longer time out for the slow CLI test

--- src/tests/streamcorpus_pipeline/test_pipeline_run.py
+++ src/tests/streamcorpus_pipeline/test_pipeline_run.py
@@ -1,9 +1,9 @@
 import os
+import time
 import pytest
 import subprocess
 from streamcorpus_pipeline._logging import logger
 
-@pytest.mark.timeout(300)
 @pytest.mark.parametrize(
     ('script_path', 'expect_success'), [
         ('john-smith-simple.sh', True), 
@@ -17,13 +17,17 @@ def test_pipeline_run(script_path, expect_success):
     logger.critical(cmd)
     p = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     ret = None
-    while 1:
+    start_time = time.time()
+    while time.time() - start_time < 300:
         ret = p.poll()
         if ret is not None:
             break
         out, err = p.communicate()
         logger.critical( out )
         logger.critical( err )
+
+    if time.time() - start_time >= 900:
+        raise Exception('timed out after %d seconds' % (time.time() - start_time))
 
     if expect_success:
         assert ret == 0
