77c1cef05e99fa7a90bcb80fdd754960b5761dc5
John R. Frank <jrf@computableinsights.com>
2013-12-03 06:23:31 -0500

    making the john-smith-lingpipe.sh test only change its final output file with an atomic rename, so other tests can use that same data file.

--- src/tests/streamcorpus_pipeline/john-smith-lingpipe.sh
+++ src/tests/streamcorpus_pipeline/john-smith-lingpipe.sh
@@ -21,12 +21,15 @@ git checkout -- data/john-smith/john-smith-tagged-by-lingpipe-0.sc
 rm -f data/john-smith/john-smith-tagged-by-lingpipe-test-0.sc
 mkdir -p $TMP
 
-echo data/john-smith/original | python -m streamcorpus_pipeline.run configs/john-smith-lingpipe.yaml
+echo data/john-smith/original | python -m streamcorpus_pipeline.run configs/john-smith-lingpipe.yaml  ## creates data/john-smith/john-smith-tagged-by-lingpipe-0-prebuild.sc
 echo data/john-smith/john-smith-0.sc | python -m streamcorpus_pipeline.run configs/john-smith-lingpipe-from-chunk.yaml
 
 ## compare dumps
-python -m streamcorpus.dump data/john-smith/john-smith-tagged-by-lingpipe-0.sc      --tokens > $TMP/lp-0.tsv
+python -m streamcorpus.dump data/john-smith/john-smith-tagged-by-lingpipe-0-prebuild.sc --tokens > $TMP/lp-0.tsv
 python -m streamcorpus.dump data/john-smith/john-smith-tagged-by-lingpipe-test-0.sc --tokens > $TMP/lp-test-0.tsv
 
 diff $TMP/lp-0.tsv $TMP/lp-test-0.tsv || die "failed: configs/john-smith-lingpipe.yaml"
 diff $TMP/lp-0.tsv data/john-smith/john-smith-tagged-by-lingpipe-0.tsv || die "failed: configs/john-smith-lingpipe-from-chunk.yaml"
+
+## atomic rename to official location
+mv data/john-smith/john-smith-tagged-by-lingpipe-0-prebuild.sc data/john-smith/john-smith-tagged-by-lingpipe-0.sc
