d22816dc950d2ffaecd3c6baf5a4c4ba07dcfa8c
John R. Frank <jrf@diffeo.com>
2014-01-09 04:08:55 +0000

    improving tests for kvlayer extractor/loader stages and adding doc_id-->epoch_ticks index for querying

--- src/streamcorpus_pipeline/_kvlayer.py
+++ src/streamcorpus_pipeline/_kvlayer.py
@@ -5,6 +5,7 @@ This software is released under an MIT/X11 open source license.
 
 Copyright 2012-2013 Diffeo, Inc.
 '''
+from collections import deque
 import os
 import sys
 import uuid
@@ -15,6 +16,16 @@ import streamcorpus
 logger = logging.getLogger(__name__)
 
 class from_kvlayer(object):
+    '''
+    loads StreamItems from a kvlayer table based an i_str passed to
+    __call__, where the i_str must be a four-tuple joined on commas
+    that provides (epoch_ticks_1, doc_id_1, epoch_ticks_2, doc_id_2)  
+
+    If no values are specified, it defaults to the entire table.  If
+    one of the epoch_ticks is provided, then both must be provided.
+    If one of the doc_ids is provided, then both must be provided.
+    That is, half-open ranges are not supported.
+    '''
     def __init__(self, config):
         self.config = config
         self.client = kvlayer.client(config)
@@ -22,33 +33,53 @@ class from_kvlayer(object):
             dict(stream_items=2))
 
     def __call__(self, i_str):
-        epoch_ticks_1, doc_id_1, epoch_ticks_2, doc_id_2 = i_str.split(',')
-        epoch_ticks_1 = uuid.UUID(int=int(epoch_ticks_1))
-        epoch_ticks_2 = uuid.UUID(int=int(epoch_ticks_2))
-        if doc_id_1:
-            assert doc_id_2, (doc_id_1, doc_id_2)
-            doc_id_1 = uuid.UUID(hex=doc_id_1)
-            doc_id_2 = uuid.UUID(hex=doc_id_2)
-            key1 = (epoch_ticks_1, doc_id_1)
-            key2 = (epoch_ticks_2, doc_id_2)
+        if i_str:
+            epoch_ticks_1, doc_id_1, epoch_ticks_2, doc_id_2 = i_str.split(',')
+            epoch_ticks_1 = uuid.UUID(int=int(epoch_ticks_1))
+            epoch_ticks_2 = uuid.UUID(int=int(epoch_ticks_2))
+            if doc_id_1:
+                assert doc_id_2, (doc_id_1, doc_id_2)
+                doc_id_1 = uuid.UUID(hex=doc_id_1)
+                doc_id_2 = uuid.UUID(hex=doc_id_2)
+                key1 = (epoch_ticks_1, doc_id_1)
+                key2 = (epoch_ticks_2, doc_id_2)
+            else:
+                key1 = (epoch_ticks_1, )
+                key2 = (epoch_ticks_2, )
+            key_ranges = [(key1, key2)]
         else:
-            key1 = (epoch_ticks_1, )
-            key2 = (epoch_ticks_2, )
+            key_ranges = []
 
-        for key, data in self.client.scan( 'stream_items', (key1, key2) ):
+        for key, data in self.client.scan( 'stream_items', *key_ranges ):
             errors, data = streamcorpus.decrypt_and_uncompress(data)
             yield streamcorpus.deserialize(data)
 
 
 class to_kvlayer(object):
+    '''
+    stores StreamItems in a kvlayer table called "stream_items" in the
+    namespace specified in the config dict for this stage.
+    
+    each StreamItem is serialized and xz compressed and stored on the
+    key (UUID(int=epoch_ticks), UUID(hex=doc_id))
+
+    This key structure supports time-range queries directly on the
+    stream_items table.
+
+    To search by doc_id, one can look up all epoch_ticks for a given
+    doc_id using the index table stream_items_doc_id_epoch_ticks,
+    which has the keys reversed and no data.
+    '''
     def __init__(self, config):
         self.config = config
         self.client = kvlayer.client(config)
         self.client.setup_namespace(
-            dict(stream_items=2))
+            dict(stream_items=2,
+                 stream_items_doc_id_epoch_ticks=2))
 
     def __call__(self, t_path, name_info, i_str):
-        def _keys_and_values():
+        inverted_keys = deque()
+        def keys_and_values():
             total_mb = 0.
             for si in streamcorpus.Chunk(t_path):
                 key1 = uuid.UUID(int=si.stream_time.epoch_ticks)
@@ -59,5 +90,8 @@ class to_kvlayer(object):
                 total_mb += float(len(data)) / 2**20
                 logger.info('%r, %r --> %d, %.3f', key1, key2, len(data), total_mb)
                 yield (key1, key2), data
+                inverted_keys.append( ((key2, key1), r'') )
 
-        self.client.put( 'stream_items', *_keys_and_values())
+        self.client.put( 'stream_items', *keys_and_values())
+
+        self.client.put( 'stream_items_doc_id_epoch_ticks', *inverted_keys)
--- src/tests/streamcorpus_pipeline/test_kvlayer.py
+++ src/tests/streamcorpus_pipeline/test_kvlayer.py
@@ -21,7 +21,7 @@ def config(request):
         thrift_framed_transport_size_in_mb=15,
     )
     def fin():
-        client = kvlayer.client(config)        
+        client = kvlayer.client(config)
         client.delete_namespace()
     request.addfinalizer(fin)
     return config
@@ -29,16 +29,37 @@ def config(request):
 def test_kvlayer_extractor_and_loader(config):
     path = get_test_v0_3_0_chunk_path()
     loader = to_kvlayer(config)
-    loader(path, {}, '')
+    
+    ## name_info and i_str are not used by the loader
+    i_str = ''
+    name_info = {}
+    loader(path, name_info, i_str)
+
+    ## check that index table was created
+    all_doc_ids = set()
+    all_epoch_ticks = set()
+    for (doc_id, epoch_ticks), empty_data in loader.client.scan('stream_items_doc_id_epoch_ticks'):
+        all_doc_ids.add(doc_id)
+        all_epoch_ticks.add(epoch_ticks)
+    all_doc_ids = sorted(all_doc_ids)
+    all_epoch_ticks = sorted(all_epoch_ticks)
+    logger.info('%d doc_ids', len(all_doc_ids))
+
+    ## make an extractor
     extractor = from_kvlayer(config)
-    stream_ids = []
-    for si in extractor( '0,,%d,' % 10**10 ):
-        stream_ids.append(si.stream_id)    
-    _input_chunk_ids = [si.stream_id for si in streamcorpus.Chunk(path)]
-    input_chunk_ids = list(set(_input_chunk_ids))
-    logger.info('%d inserts, %d unique',
-                len(_input_chunk_ids), len(input_chunk_ids))
-    input_chunk_ids.sort()
-    stream_ids.sort()
-    assert len(input_chunk_ids) == len(stream_ids)
-    assert input_chunk_ids == stream_ids
+
+    ## test it with different i_str inputs:
+    for i_str in ['', '0,,%d,' % 10**10, '%d,%s,%d,%s' % (all_epoch_ticks[0],  all_doc_ids[0],
+                                                          all_epoch_ticks[-1], all_doc_ids[-1]) ]:
+        stream_ids = []
+        for si in extractor(i_str):
+            stream_ids.append(si.stream_id)    
+        _input_chunk_ids = [si.stream_id for si in streamcorpus.Chunk(path)]
+        input_chunk_ids = list(set(_input_chunk_ids))
+        logger.info('%d inserts, %d unique',
+                    len(_input_chunk_ids), len(input_chunk_ids))
+        input_chunk_ids.sort()
+        stream_ids.sort()
+        assert len(input_chunk_ids) == len(stream_ids)
+        assert input_chunk_ids == stream_ids
+
