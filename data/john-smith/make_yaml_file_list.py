bcd63a2ad932e10eff55f255c04f356e230d2d94
John R. Frank <jrf@diffeo.com>
2014-02-05 00:38:34 +0000

    fixing make_yaml_files_list to work from outside the john-smith/original dir, and making john-smith-serif.yaml use the reader that can take the output of this script as its -i input.

--- data/john-smith/make_yaml_file_list.py
+++ data/john-smith/make_yaml_file_list.py
@@ -7,15 +7,24 @@ python make_yaml_file_list.py  > john_smith_files.yaml
 
 streamcorpus_pipeline ../../../configs/john-smith-serif.yaml  -i john_smith_files.yaml 
 """
+import argparse
 
 import os
 import sys
 
 import yaml
 
+def log(m):
+    sys.stderr.write(m)
+    sys.stderr.write('\n')
+    sys.stderr.flush()
+
 
 if __name__ == '__main__':
-    base_path = os.getcwd()
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument('base_path', help='path to "original" John Smith corpus directory containing a subdirectory for each entity')
+    args = parser.parse_args()
+
     metadata = dict()
     #metadata['abs_url_base'] = base_path
     metadata['source'] = 'bagga-and-baldwin'
@@ -23,13 +32,12 @@ if __name__ == '__main__':
     #metadata['root_path'] = base_path
 
     entries = []
-    for fname in os.listdir(base_path):
-        if fname[0] == '.':
-            continue
-        if os.path.isdir(fname):
+    for fname in os.listdir(args.base_path):
+        fpath = os.path.join(args.base_path, fname)
+        if os.path.isdir(fpath):
             entry = {
                 'target_id': fname,
-                'doc_path': fname + '/',
+                'doc_path': os.path.abspath(fpath + '/'),
                 'mentions': ['John Smith'],
                 }
             entries.append(entry)
