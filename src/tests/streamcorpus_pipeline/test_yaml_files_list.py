86fb74d808e9637e061253a3bbc8e6237eca71ba
Steven Bauer <bauer@diffeo.com>
2014-02-17 21:36:56 +0000

    Demonstrate profile_path also works

--- src/tests/streamcorpus_pipeline/test_yaml_files_list.py
+++ src/tests/streamcorpus_pipeline/test_yaml_files_list.py
@@ -6,10 +6,12 @@ from streamcorpus_pipeline._yaml_files_list import yaml_files_list
 def test_parse_file():
     yfl = yaml_files_list({})
     input_file = os.path.join(os.path.dirname(__file__), '../../../data/john-smith/ground-truth.yaml')
-
+    cnt = 0
     for si in yfl(input_file):
         assert si.stream_id
         assert si.body.raw
+        cnt += 1
+    assert cnt == 197
 
 def test_parse_mentions():
     raw_mentions = ['John Smith', {'name': 'John Smith'}, {'ip_address': '10.0.0.1'}]
