aa54043eef644e2104f244c56441cc5865d3ecef
Steven Bauer <bauer@diffeo.com>
2014-03-11 18:29:08 +0000

    Make factory public and fix a bug
    Factory can be used to construct pipelines.
    
    Also only yield documents once.  Because the user can specify
    profile_paths and doc_paths we have the possibility of
    duplicates.

--- streamcorpus_pipeline/__init__.py
+++ streamcorpus_pipeline/__init__.py
@@ -5,5 +5,5 @@ Copyright 2012-2014 Diffeo, Inc.
 '''
 from config import config_name, default_config, runtime_keys, sub_modules, \
     replace_config, check_config
-from _pipeline import Pipeline
+from _pipeline import Pipeline, PipelineFactory
 from _rejester import rejester_run_function, rejester_terminate_function
