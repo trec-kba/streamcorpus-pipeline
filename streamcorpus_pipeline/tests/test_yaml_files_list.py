6f74c458dc523bc57470256e0b9b3699662b3ebf
John R. Frank <jrf@diffeo.com>
2014-03-26 03:44:06 +0000

    updating test and data file used by test for yaml file reader, so that it now respects the requirement of having a valid abs_url

--- streamcorpus_pipeline/tests/test_yaml_files_list.py
+++ streamcorpus_pipeline/tests/test_yaml_files_list.py
@@ -8,6 +8,7 @@ from streamcorpus_pipeline._yaml_files_list import yaml_files_list
 def test_parse_file():
     yfl = yaml_files_list(config={})
     input_file = os.path.join(os.path.dirname(__file__), '../../data/john-smith/ground-truth.yaml')
+    os.chdir(os.path.join(os.path.dirname(__file__), '../../data/john-smith/'))
     cnt = 0
     for si in yfl(input_file):
         assert si.stream_id
