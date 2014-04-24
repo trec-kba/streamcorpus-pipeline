e4982a800ea9f0115095a444622d4676b98b842e
John R. Frank <jrf@diffeo.com>
2014-04-24 04:00:01 +0000

    fixing the way that --third-dir gets used by streamcorpus_pipeline_test

--- streamcorpus_pipeline/tests/run.py
+++ streamcorpus_pipeline/tests/run.py
@@ -13,11 +13,15 @@ import sys
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('redis_address', help='hostname:port of a redis instance to use as kvlayer backend and registry backend.  Running the tests requires a redis instance accessible on the network.')
-    parser.add_argument('--third-dir', help='path to third-party tools directory')
+    parser.add_argument('--third-dir', help='path to third-party tools directory', default=None)
     args = parser.parse_args()
 
     test_dir = os.path.dirname(__file__)
-    response = pytest.main(['-vv', '-n', '8', '--runslow', '--runperf', '--redis-address', args.redis_address, '--third-dir', args.third_dir, test_dir])
+    cmd = ['-vv', '-n', '8', '--runslow', '--runperf', '--redis-address', args.redis_address]
+    if args.third_dir:
+        cmd.extend(['--third-dir', args.third_dir, '--run-integration'])
+    cmd.append(test_dir)
+    response = pytest.main(cmd)
     if response:
         sys.exit(response)
 
