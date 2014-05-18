facaf623450858d8071b7e18fe4b64d8048f5662
John R. Frank <jrf@diffeo.com>
2014-05-18 04:30:39 -0400

    adding `rmtree` as a thing exposed by streamcorpus_pipeline.util

--- streamcorpus_pipeline/util.py
+++ streamcorpus_pipeline/util.py
@@ -1,7 +1,8 @@
-'''
-This software is released under an MIT/X11 open source license.
+'''convenience functions from streamcorpus_pipeline 
 
-Copyright 2012-2013 Diffeo, Inc.
+.. This software is released under an MIT/X11 open source license.
+   Copyright 2012-2014 Diffeo, Inc.
 '''
 from __future__ import absolute_import
 from streamcorpus_pipeline._clean_visible import cleanse
+from streamcorpus_pipeline._rmtree import rmtree
