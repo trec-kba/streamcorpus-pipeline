4ef95df4083eb6011cdb7b964f9af8d38b8560ae
David Maze <dmaze@diffeo.com>
2014-10-10 15:49:16 -0400

    Find test data files in the right place for installed packages.
    They're under sysconfig.get_path('data'), not sys.prefix.  Canonicalize
    sys.prefix too, in some virtual environment setups it can be a relative
    path.

--- streamcorpus_pipeline/tests/conftest.py
+++ streamcorpus_pipeline/tests/conftest.py
@@ -1,18 +1,25 @@
+'''py.test hooks for streamcorpus-pipeline.'''
+from __future__ import absolute_import
 import os
 import sys
+import sysconfig
+
 import pytest
+
 
 @pytest.fixture
 def test_data_dir(request):
-    ## Determine directory where this file lives and return
-    ## different paths depending on whether or not this
-    ## conftest.py file has been installed
+    # Determine directory where this file lives and return
+    # different paths depending on whether or not this
+    # conftest.py file has been installed
     cur_directory_path = os.path.abspath(os.path.dirname(__file__))
 
-    if sys.prefix in cur_directory_path:
-        ## Running from an installed location
-        path = os.path.join(sys.prefix, 'data/streamcorpus-pipeline')
+    if cur_directory_path.startswith(os.path.abspath(sys.prefix)):
+        # Running from an installed location
+        path = os.path.join(sysconfig.get_path('data'),
+                            'data/streamcorpus-pipeline')
     else:
-        ## Running not installed
-        path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data'))
+        # Running not installed
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            '../../data'))
     return path
