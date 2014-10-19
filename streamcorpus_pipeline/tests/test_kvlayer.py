b4779456dfea68bb90570d96b15f5e66e88bf9ba
John R. Frank <jrf@diffeo.com>
2014-10-19 02:29:06 +0000

    improved doc string

--- streamcorpus_pipeline/tests/test_kvlayer.py
+++ streamcorpus_pipeline/tests/test_kvlayer.py
@@ -1,3 +1,8 @@
+'''tests for {to,from}_kvlayer reader/writer stages
+
+.. This software is released under an MIT/X11 open source license.
+   Copyright 2012-2014 Diffeo, Inc.
+'''
 from __future__ import absolute_import
 from collections import defaultdict
 import contextlib
