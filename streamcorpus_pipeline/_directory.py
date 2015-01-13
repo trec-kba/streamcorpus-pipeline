fe550ad146b794c720ac0a06e141ab9354811929
David Maze <dmaze@diffeo.com>
2015-01-13 15:24:15 -0500

    Make streamcorpus_directory correctly use alternate rejester TaskMaster.
    Style cleanups to satisfy flake8 on this file too.

--- streamcorpus_pipeline/_directory.py
+++ streamcorpus_pipeline/_directory.py
@@ -1,7 +1,7 @@
 '''Process entire directories using streamcorpus_pipeline.
 
 .. This software is released under an MIT/X11 open source license.
-   Copyright 2014 Diffeo, Inc.
+   Copyright 2014-2015 Diffeo, Inc.
 
 :command:`streamcorpus_directory` reads an entire directory through
 the streamcorpus pipeline.  This allows work to be distributed using
@@ -92,11 +92,16 @@ from streamcorpus_pipeline._rejester import rejester_run_function
 from streamcorpus_pipeline.run import SimpleWorkUnit
 import yakonfig
 
+
 class DirectoryConfig(object):
     '''Configuration metadata for the directory scanner.'''
     config_name = 'streamcorpus_directory'
-    default_config = { 'engine': 'rejester', 'mode': 'directories',
-                       'name': 'streamcorpus_directory' }
+    default_config = {
+        'engine': 'rejester',
+        'mode': 'directories',
+        'name': 'streamcorpus_directory',
+    }
+
     @staticmethod
     def add_arguments(parser):
         parser.add_argument(
@@ -121,34 +126,43 @@ class DirectoryConfig(object):
             help='arguments are files containing lists of files')
         parser.add_argument(
             '--work-spec', help='name of rejester work spec')
-    runtime_keys = { 'engine': 'engine', 'mode': 'mode', 'work_spec': 'name' }
+
+    runtime_keys = {
+        'engine': 'engine',
+        'mode': 'mode',
+        'work_spec': 'name',
+    }
+
     @staticmethod
     def check_config(config, name):
-        if ('engine' not in config or
-            config['engine'] not in ['rejester', 'standalone']):
+        if (('engine' not in config or
+             config['engine'] not in ['rejester', 'standalone'])):
             raise yakonfig.ConfigurationError(
                 'invalid {} engine type {!r}'
                 .format(name, config.get('engine')))
-        if ('mode' not in config or
-            config['mode'] not in ['directories', 'files', 'file-lists']):
+        if (('mode' not in config or
+             config['mode'] not in ['directories', 'files', 'file-lists'])):
             raise yakonfig.ConfigurationError(
                 'invalid {} mode {!r}'.format(name, config.get('mode')))
         if config['engine'] == 'rejester':
             yakonfig.check_toplevel_config(rejester, name)
         yakonfig.check_toplevel_config(streamcorpus_pipeline, name)
 
+
 def main():
-    parser = argparse.ArgumentParser(conflict_handler='resolve',
+    parser = argparse.ArgumentParser(
+        conflict_handler='resolve',
         description='process entire directories using streamcorpus_pipeline')
     parser.add_argument('directories', nargs='+', metavar='directory',
                         help='directory name(s) to process')
-    parser.add_argument('-n', '--nice', default=0, type=int, 
+    parser.add_argument('-n', '--nice', default=0, type=int,
                         help='specify a nice level for these jobs')
-    args = yakonfig.parse_args(parser, [yakonfig, rejester, kvlayer, dblogger,
-                                        streamcorpus_pipeline, DirectoryConfig])
+    args = yakonfig.parse_args(
+        parser, [yakonfig, rejester, kvlayer, dblogger,
+                 streamcorpus_pipeline, DirectoryConfig])
     gconfig = yakonfig.get_global_config()
     scdconfig = gconfig['streamcorpus_directory']
-    
+
     work_spec = {
         'name': scdconfig.get('name', 'streamcorpus_directory'),
         'desc': 'read files from a directory',
@@ -172,18 +186,18 @@ def main():
                     for filename in filenames:
                         yield os.path.abspath(os.path.join(dirpath, filename))
 
-    work_units = { filename: { 'start_count': 0 }
-                   for filename in get_filenames() }
+    work_units = {filename: {'start_count': 0}
+                  for filename in get_filenames()}
 
     if scdconfig['engine'] == 'rejester':
-        tm = rejester.TaskMaster(gconfig['rejester'])
+        tm = rejester.build_task_master(gconfig['rejester'])
         tm.update_bundle(work_spec, work_units, nice=args.nice)
     elif scdconfig['engine'] == 'standalone':
-        for k,v in work_units.iteritems():
+        for k, v in work_units.iteritems():
             u = SimpleWorkUnit(k)
             u.spec = work_spec
             u.data = v
             rejester_run_function(u)
-    
+
 if __name__ == '__main__':
     main()
