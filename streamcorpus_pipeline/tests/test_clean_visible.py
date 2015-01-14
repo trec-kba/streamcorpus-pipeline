2bad7b18ae9aebd8bf8f053a9dcb3de5ddca3e64
David Maze <dmaze@diffeo.com>
2015-01-14 16:08:23 -0500

    Also improve clean_visible.
    Improve the state-machine-based implementation to read blocks of contiguous
    characters, rather than one character at a time.  This is again a pretty
    measurable performance gain.  Write actual tests for the HTML cleanup.

--- streamcorpus_pipeline/tests/test_clean_visible.py
+++ streamcorpus_pipeline/tests/test_clean_visible.py
@@ -1,7 +1,49 @@
-import os
-import pytest
+from __future__ import absolute_import
+from streamcorpus_pipeline._clean_visible import cleanse, make_clean_visible
 
-from streamcorpus_pipeline._clean_visible import cleanse
 
 def test_cleanse():
-    assert cleanse(u'This -LRB-big-RRB- dog has no \u1F601 Teeth') == u'this big dog has no \u1F601 teeth'
+    assert (cleanse(u'This -LRB-big-RRB- dog has no \u1F601 Teeth') ==
+            u'this big dog has no \u1F601 teeth')
+
+
+def test_make_clean_visible_simple():
+    s = 'The quick brown fox jumped over the lazy dog.'
+    t = 'The quick brown fox jumped over the lazy dog.'
+    u = make_clean_visible(s)
+    assert t == u
+
+
+def test_make_clean_visible_tag():
+    s = 'The <i>quick</i> brown fox <b>jumped</b> over the lazy dog.'
+    t = 'The    quick     brown fox    jumped     over the lazy dog.'
+    u = make_clean_visible(s)
+    assert t == u
+
+
+def test_make_clean_visible_nl():
+    s = 'The <i>quick\nbrown\nfox</i> jumped\nover the <b>lazy\rdog</b>.'
+    t = 'The    quick\nbrown\nfox     jumped\nover the    lazy\rdog    .'
+    u = make_clean_visible(s)
+    assert t == u
+
+
+def test_make_clean_visible_embedded_nl():
+    s = 'The <a href="fox#quick">fox</a> <a\nhref="jump">jumped</a>.'
+    t = 'The                     fox       \n            jumped    .'
+    u = make_clean_visible(s)
+    assert t == u
+
+
+def test_make_clean_visible_script():
+    s = 'The <script><![CDATA[crafty]]></script> fox jumped.'
+    t = 'The                                     fox jumped.'
+    u = make_clean_visible(s)
+    assert t == u
+
+
+def test_make_clean_visible_script_2():
+    s = 'The <script>crafty</script> fox jumped.'
+    t = 'The         crafty          fox jumped.'
+    u = make_clean_visible(s)
+    assert t == u
