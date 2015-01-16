f9f3739223c35efb90dc266f42cd7e663d506290
David Maze <dmaze@diffeo.com>
2015-01-16 11:44:53 -0500

    Clean up nltk_tokenizer, write real tests for it.

--- streamcorpus_pipeline/_tokenizer.py
+++ streamcorpus_pipeline/_tokenizer.py
@@ -1,60 +1,67 @@
-#!/usr/bin/env python
-'''
-streamcorpus_pipeline.TaggerBatchTransform for LingPipe
+'''Pipeline transform for :mod:`nltk`.
 
-This software is released under an MIT/X11 open source license.
+.. This software is released under an MIT/X11 open source license.
+   Copyright 2012-2015 Diffeo, Inc.
 
-Copyright 2012-2014 Diffeo, Inc.
+.. autoclass:: nltk_tokenizer
+
 '''
 from __future__ import absolute_import
-import hashlib
-import itertools
 import logging
-import os
-import re
-import sys
-import time
-import traceback
-import uuid
 
 from nltk.tokenize import WhitespaceTokenizer
-from nltk.tokenize.punkt import PunktWordTokenizer, PunktSentenceTokenizer
+from nltk.tokenize.punkt import PunktSentenceTokenizer
 
 from sortedcollection import SortedCollection
 import streamcorpus
-from streamcorpus import Token, Sentence, EntityType, Chunk, Offset, OffsetType, Gender, MentionType, Attribute, AttributeType
+from streamcorpus import Token, Sentence, Offset, OffsetType
 from streamcorpus_pipeline.stages import IncrementalTransform
-from streamcorpus_pipeline._taggers import TaggerBatchTransform
 
 logger = logging.getLogger(__name__)
 
+
 class nltk_tokenizer(IncrementalTransform):
-    '''
-    a streamcorpus_pipeline IncrementalTransform that converts a chunk into a new
-    chunk with Sentence objects generated using NLTK tokenizers
+    '''Minimal tokenizer using :mod:`nltk`.
+
+    This is an incremental transform that creates minimal tokens and
+    does minimal label alignment.  The stream item must have a
+    `clean_visible` part already.  The stream item is tagged with
+    ``nltk_tokenizer``.
+
+    This tokenizer has very few dependencies, only the :mod:`nltk`
+    Python package.  However, the tokens that it creates are
+    extremely minimal, with no part-of-speech details or other
+    information.  The tokens only contain the `token` property and
+    character-offset information.
+
+    If there are labels with character offsets attached to the stream
+    item body, and this stage is configured with an `annotator_id`,
+    then this stage attempts to attach those labels to tokens.
+
     '''
     config_name = 'nltk_tokenizer'
     tagger_id = 'nltk_tokenizer'
-    def __init__(self, *args, **kwargs):
-        super(nltk_tokenizer, self).__init__(*args, **kwargs)
+
+    def __init__(self, config):
+        super(nltk_tokenizer, self).__init__(config)
         self.sentence_tokenizer = PunktSentenceTokenizer()
-        self.word_tokenizer = WhitespaceTokenizer() #PunktWordTokenizer()
+        self.word_tokenizer = WhitespaceTokenizer()  # PunktWordTokenizer()
+        self.annotator_id = self.config.get('annotator_id', None)
 
     def _sentences(self, clean_visible):
         'generate strings identified as sentences'
         previous_end = 0
         clean_visible = clean_visible.decode('utf8')
-        assert isinstance(clean_visible, unicode)
         for start, end in self.sentence_tokenizer.span_tokenize(clean_visible):
-            ## no need to check start, because the first byte of text
-            ## is always first byte of first sentence, and we will
-            ## have already made the previous sentence longer on the
-            ## end if there was an overlap.
-            if  start < previous_end:
+            # no need to check start, because the first byte of text
+            # is always first byte of first sentence, and we will
+            # have already made the previous sentence longer on the
+            # end if there was an overlap.
+            if start < previous_end:
                 start = previous_end
                 if start > end:
-                    ## skip this sentence... because it was eaten by
-                    ## an earlier sentence with a label
+                    # skip this sentence... because it was eaten by
+                    # an earlier sentence with a label
                     continue
             try:
                 label = self.label_index.find_le(end)
@@ -69,12 +76,12 @@ class nltk_tokenizer(IncrementalTransform):
 
     def make_label_index(self, stream_item):
         'make a sortedcollection on body.labels'
-        labels = stream_item.body.labels.get(self.config.get('annotator_id'))
+        labels = stream_item.body.labels.get(self.annotator_id)
         if not labels:
             labels = []
 
         self.label_index = SortedCollection(
-            labels,
+            [l for l in labels if OffsetType.CHARS in l.offsets],
             key=lambda label: label.offsets[OffsetType.CHARS].first)
 
     def make_sentences(self, stream_item):
@@ -83,7 +90,8 @@ class nltk_tokenizer(IncrementalTransform):
         sentences = []
         token_num = 0
         new_mention_id = 0
-        for sent_start, sent_end, sent_str in self._sentences(stream_item.body.clean_visible):
+        for sent_start, sent_end, sent_str in self._sentences(
+                stream_item.body.clean_visible):
             assert isinstance(sent_str, unicode)
             sent = Sentence()
             sentence_pos = 0
@@ -95,26 +103,21 @@ class nltk_tokenizer(IncrementalTransform):
                     sentence_pos=sentence_pos,
                 )
                 tok.offsets[OffsetType.CHARS] = Offset(
-                    type=OffsetType.CHARS, 
+                    type=OffsetType.CHARS,
                     first=sent_start + start,
-                    length = end - start,
+                    length=end - start,
                 )
-                ## whitespace tokenizer will never get a token
-                ## boundary in the middle of an 'author' label
+                # whitespace tokenizer will never get a token
+                # boundary in the middle of an 'author' label
                 try:
-                    #logger.debug('searching for %d in %r', sent_start + start, self.label_index._keys)
                     label = self.label_index.find_le(sent_start + start)
                 except ValueError:
                     label = None
                 if label:
                     off = label.offsets[OffsetType.CHARS]
                     if off.first + off.length > sent_start + start:
-                        logger.info('overlapping label: %r' % label.target.target_id)
-                        ## overlaps
                         streamcorpus.add_annotation(tok, label)
-                        assert label.annotator.annotator_id in tok.labels
-
-                        logger.info('adding label to tok: %r has %r',
+                        logger.debug('adding label to tok: %r has %r',
                                      tok.token, label.target.target_id)
 
                         if label in self.label_to_mention_id:
@@ -133,44 +136,12 @@ class nltk_tokenizer(IncrementalTransform):
         return sentences
 
     def process_item(self, stream_item, context=None):
-        if not hasattr(stream_item.body, 'clean_visible') or not stream_item.body.clean_visible:
+        if not stream_item.body.clean_visible:
             return stream_item
-            
+
         self.label_index = None
         self.label_to_mention_id = dict()
-        stream_item.body.sentences[self.tagger_id] = self.make_sentences(stream_item)
+        sentences = self.make_sentences(stream_item)
+        stream_item.body.sentences[self.tagger_id] = sentences
 
         return stream_item
-
-    def __call__(self, stream_item, context=None):
-        ## support the legacy callable API
-        return self.process_item(stream_item, context)
-        
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input')
-    args = parser.parse_args()
-
-    logger = logging.getLogger('streamcorpus_pipeline')
-    ch = logging.StreamHandler()
-    logger.addHandler(ch)
-    streamcorpus_logger = logging.getLogger('streamcorpus')
-    streamcorpus_logger.addHandler(ch)
-
-    t = nltk_tokenizer(dict(annotator_id='author'))
-    for si in Chunk(args.input):
-        t.process_item(si)
-        if si.body.clean_visible:
-            assert len(si.body.sentences['nltk_tokenizer']) > 0
-            logger.critical( 'num_sentences=%d, has wikipedia %r and %d labels',
-                             len(si.body.sentences['nltk_tokenizer']),  
-                             'wikipedia.org' in si.body.clean_html,
-                             len(getattr(si.body, 'labels', {}).get('author', {})) )
-
-            #if len(getattr(si.body, 'labels', {}).get('author', {})) > 3:
-            #    c = Chunk('foo.sc', mode='wb')
-            #    c.add(si)
-            #    c.close()
-            #    sys.exit()
-        
