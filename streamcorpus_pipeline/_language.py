ab11ad0a3c4d53959040e47c540d59c59bf5cb63
John R. Frank <jrf@diffeo.com>
2015-05-04 04:14:37 +0000

    improving language transform and fixing DIFFEO-1302
    Previously, the language transform was just blindly feeding
    si.body.raw into cld.detect, even though cld.detect expects a utf8
    string.  For data that was not already in utf8 this would often not
    produce an is_reliable=True response, resulting in
    si.body.language.{code,name}=None
    
    This new version tries clean_html, clean_visible, and then attempts to
    properly decode body.raw using body.encoding, and only falls back to
    an undecoded body.raw as a final resort.  That is, the previous
    behavior is now the last resort.

--- streamcorpus_pipeline/_language.py
+++ streamcorpus_pipeline/_language.py
@@ -9,34 +9,68 @@ Copyright 2012-2014 Diffeo, Inc.
 
 from __future__ import absolute_import
 import cld
+import logging
 from streamcorpus import Language
 from streamcorpus_pipeline.stages import Configured
 
+logger = logging.getLogger(__name__)
+
 class language(Configured):
-    '''Guess at a language from ``body.raw``.
+    '''Guess at a language from ``body``.
 
     This always adds a ``language`` annotation to the body, but if
     the body does not have a raw part or the language cannot be reliably
     detected, it may be empty.
 
+    This attempts to detect on ``body.clean_html`` if present, and
+    then ``body.clean_visible`` if present, and then falls back to
+    attempting to use ``body.encoding`` to decode the ``body.raw`` and
+    tries to detect language from the output of that, and finally, it
+    attempts to detect language on undecoded ``body.raw``.
+
     This has no configuration options.
+
     '''
     config_name = 'language'
     def __call__(self, si, context):
-        if si.body and si.body.raw:
-            name, code, is_reliable, num_text_bytes, details = cld.detect(si.body.raw)
-            if is_reliable and code != 'xxx':
-                si.body.language = Language(code=code, name=name)
-            else:
-                si.body.language = Language(code='', name='')
+        if not si.body:
+            return si
 
-        elif si.body:
-            ## no .body.raw -- rare, but not impossible
-            si.body.language = Language(code='', name='')
+        b = si.body
+        is_reliable = False
+        code = None
+        name = None
 
-        if 'force' in self.config:            
-            si.body.language = Language(
-                code=self.config['force'].get('code'), 
-                name=self.config['force'].get('name'))
+        if ((not is_reliable) or code == 'xxx') and b.clean_html:
+            name, code, is_reliable, num_text_bytes, details = \
+                cld.detect(b.clean_html)
+
+        if ((not is_reliable) or code == 'xxx') and b.clean_visible:
+            name, code, is_reliable, num_text_bytes, details = \
+                cld.detect(b.clean_visible)
+
+        if ((not is_reliable) or code == 'xxx') and b.encoding and b.raw:
+            raw = None
+            try:
+                raw = b.raw.decode(b.encoding, 'ignore')\
+                                 .encode('utf8', 'ignore')
+            except Exception, exc:
+                logger.warn('failed to decode body.raw using body.encoding=%r',
+                            b.encoding, exc_info=True)
+            if raw:
+                name, code, is_reliable, num_text_bytes, details = cld.detect(raw)
+
+        if ((not is_reliable) or code == 'xxx') and b.raw:
+            name, code, is_reliable, num_text_bytes, details = cld.detect(b.raw)
+
+        # always create `body.language`
+        b.language = Language(code='', name='')
+        if is_reliable and code != 'xxx':
+            b.language.code = code
+            b.language.name = name
+
+        if 'force' in self.config:
+            b.language.code = self.config['force'].get('code')
+            b.language.name = self.config['force'].get('name')
 
         return si
--- streamcorpus_pipeline/tests/test_language.py
+++ streamcorpus_pipeline/tests/test_language.py
@@ -5,7 +5,9 @@
 '''
 from __future__ import absolute_import
 import os
+import pytest
 import streamcorpus_pipeline
+from streamcorpus_pipeline._clean_html import clean_html
 from streamcorpus_pipeline._language import language
 from streamcorpus import make_stream_item, ContentItem
 
@@ -19,3 +21,23 @@ def test_langauge(test_data_dir):
 
     assert si.body.language.name == 'Japanese'
     assert si.body.language.code == 'ja'
+
+
+@pytest.mark.parametrize('with_clean_html', [(True,), (False,)])
+def test_language_unreliable_on_raw(test_data_dir, with_clean_html):
+    path = os.path.join(test_data_dir, 'test/unreliable-language-detect-on-raw.html')
+    si = make_stream_item(None, 'http://bbs.sjtu.edu.cn/bbsanc?path=%2Fgroups%2FGROUP_0%2Fmessage%2FD4EFC2634%2FD7AC8E3A8%2FG.1092960050.A')
+    raw = open(path).read()
+    #raw = raw.decode('GB2312', 'ignore').encode('utf8')
+    si.body = ContentItem(raw=raw)
+    si.body.encoding = 'GB2312'
+    si.body.media_type = 'text/html'
+    context = {}
+    if with_clean_html:
+        ch = clean_html(config={})
+        ch(si, context)
+    lang = language(config={})
+    lang(si, context)
+
+    assert si.body.language.name == 'Chinese'
+    assert si.body.language.code == 'zh'
